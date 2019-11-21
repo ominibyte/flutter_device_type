f10b34b8dba145c5e4501925390504239f3d873b
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2019-11-21 09:30:18 -0300

    Add hasNotch property

--- test/flutter_device_type_test.dart
+++ test/flutter_device_type_test.dart
@@ -1,10 +1,10 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_device_type/flutter_device_type.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Test that size is not null', () {
     expect(Device.size, isNotNull);
     expect(Device.get().isIphoneX, isNotNull);
+    expect(Device.get().hasNotch, isNotNull);
   });
 }
