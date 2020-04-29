b87181dddf3025276ded16b78c5d32a38513cbe1
Raka Khairil <raka.khairil@gmail.com>
2020-04-29 13:06:10 +0700

    Handle tablet with pixel ratio > 2

--- lib/flutter_device_type.dart
+++ lib/flutter_device_type.dart
@@ -3,7 +3,7 @@ library flutter_device_type;
 import 'dart:ui' as ui;
 import 'dart:io';
 
-class Device{
+class Device {
   static double devicePixelRatio = ui.window.devicePixelRatio;
   static ui.Size size = ui.window.physicalSize;
   static double width = size.width;
@@ -16,9 +16,8 @@ class Device{
 
   Device({this.isTablet, this.isPhone, this.isIos, this.isAndroid, this.isIphoneX});
 
-  factory Device.get(){
-    if( _device != null )
-      return _device;
+  factory Device.get() {
+    if (_device != null) return _device;
 
     bool isTablet;
     bool isPhone;
@@ -26,28 +25,26 @@ class Device{
     bool isAndroid = Platform.isAndroid;
     bool isIphoneX = false;
 
-    if(devicePixelRatio < 2 && (width >= 1000 || height >= 1000)) {
+    if (devicePixelRatio < 2 && (width >= 1000 || height >= 1000)) {
       isTablet = true;
       isPhone = false;
-    }
-    else if(devicePixelRatio == 2 && (width >= 1920 || height >= 1920)) {
+    } else if (devicePixelRatio >= 2 && (width >= 1920 || height >= 1920)) {
       isTablet = true;
       isPhone = false;
-    }
-    else {
+    } else {
       isTablet = false;
       isPhone = true;
     }
 
-    if( isIos && (screenHeight == 812 || screenWidth == 812 || screenHeight == 896 || screenWidth == 896) )
+    if (isIos && (screenHeight == 812 || screenWidth == 812 || screenHeight == 896 || screenWidth == 896))
       isIphoneX = true;
 
     return _device = new Device(
-        isTablet: isTablet,
-        isPhone: isPhone,
-        isAndroid: isAndroid,
-        isIos: isIos,
-        isIphoneX: isIphoneX
+      isTablet: isTablet,
+      isPhone: isPhone,
+      isAndroid: isAndroid,
+      isIos: isIos,
+      isIphoneX: isIphoneX,
     );
   }
 }
